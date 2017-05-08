6c9cdad677dd019ee1005072a4920de7a66f2fd6
paep3nguin <paep3nguin@gmail.com>
2017-05-08 10:36:14 -0500

    Remove unused drag drop code in summary view

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -83,45 +83,6 @@ angular
       $location.path(tab);
     }
 
-    $scope.dragStart = function(e, ui) {
-      ui.item.data('start', ui.item.index());
-      console.log(ui.item.index());
-
-    }// create a temporary attribute on the element with old index
-
-    $scope.dragEnd = function(e, ui) {
-      console.log('dragend');
-
-      var start = ui.item.data('start'),
-          end = ui.item.index();//get the new and old index
-
-      if (start<end) {
-        $scope.teamList[start].rank = end+1;
-        for (var i=start+1; i<=end;i++) {
-          $scope.teamList[i].rank-=1;
-        }
-      }
-      else {
-        $scope.teamList[start].rank = end+1;
-        for (var i=end; i<start;i++) {
-          $scope.teamList[i].rank+=1;
-        }
-      }
-
-      var teamsRef = new Firebase(ref.child("teams"));
-
-      for (var i=0; i<$scope.teamList.length;i++) {
-        $scope.teamList.$save(i);
-      }
-
-      $scope.teamList.sort(function(a,b) {return a.rank-b.rank})
-    }
-
-    $('#sortable').sortable({
-      start: $scope.dragStart,
-      update: $scope.dragEnd
-    });
-
     var calcAvgRank = function(team) {
       var rank;
       var ranksum = 0;
