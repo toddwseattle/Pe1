5cd0fa2d02b79b4eb5f502dd6fd0eeac100b3ae2
paep3nguin <paep3nguin@gmail.com>
2017-05-31 21:21:43 -0500

    Reindent a few files

--- app/judge/judge.js
+++ app/judge/judge.js
@@ -1,78 +1,78 @@
 'use strict';
 angular
-  .module('PitchEvaluator')
-  .controller('JudgeCtrl', function ($timeout, $rootScope, $scope, permissionsService, $firebaseObject, $firebaseArray, $location, loggedinCheck, teamService) {
-    loggedinCheck.check();
+	.module('PitchEvaluator')
+	.controller('JudgeCtrl', function ($timeout, $rootScope, $scope, permissionsService, $firebaseObject, $firebaseArray, $location, loggedinCheck, teamService) {
+		loggedinCheck.check();
 
-    if (!permissionsService.isPermitted('JudgeView')) {
-      if ($rootScope.role == 'Team') {
-        $location.path('team');
-      }
-    }
+		if (!permissionsService.isPermitted('JudgeView')) {
+			if ($rootScope.role == 'Team') {
+				$location.path('team');
+			}
+		}
 
-    $scope.reviewTeam = function (team) {
-      teamService.set(team);
-      $location.path('review');
-    }
+		$scope.reviewTeam = function (team) {
+			teamService.set(team);
+			$location.path('review');
+		}
 
-    $scope.sessionAverages = $firebaseObject(firebase.database().ref($rootScope.sessionRef).child('averages'));
-    $scope.reviewedTeams = [];
-    $scope.unReviewedTeams = [];
-    var teamsRef = firebase.database().ref($rootScope.sessionRef + "/teams");
-    var teamList = $firebaseArray(teamsRef);
-    teamList.$loaded(function () {
-      for (var i = 0; i < teamList.length; i++) {
-        var team = teamList[i];
-        if (team.reviews && team.reviews[$rootScope.user]) {
-          // We've reviewed this team
-          $scope.reviewedTeams.push(team);
-        } else {
-          // We haven't reviewed this team
-          $scope.unReviewedTeams.push(team);
-        }
-      }
-      $scope.reviewedTeams.sort(function (a, b) { return a.reviews[$rootScope.user].rank - b.reviews[$rootScope.user].rank });
-    });
+		$scope.sessionAverages = $firebaseObject(firebase.database().ref($rootScope.sessionRef).child('averages'));
+		$scope.reviewedTeams = [];
+		$scope.unReviewedTeams = [];
+		var teamsRef = firebase.database().ref($rootScope.sessionRef + "/teams");
+		var teamList = $firebaseArray(teamsRef);
+		teamList.$loaded(function () {
+			for (var i = 0; i < teamList.length; i++) {
+				var team = teamList[i];
+				if (team.reviews && team.reviews[$rootScope.user]) {
+					// We've reviewed this team
+					$scope.reviewedTeams.push(team);
+				} else {
+					// We haven't reviewed this team
+					$scope.unReviewedTeams.push(team);
+				}
+			}
+			$scope.reviewedTeams.sort(function (a, b) { return a.reviews[$rootScope.user].rank - b.reviews[$rootScope.user].rank });
+		});
 
-    function dragStart(e, ui) {
-      // Create a temporary attribute on the element with old index
-      ui.item.data('start', ui.item.index());
-    }
+		function dragStart(e, ui) {
+			// Create a temporary attribute on the element with old index
+			ui.item.data('start', ui.item.index());
+		}
 
-    function dragEnd(e, ui) {
-      // Get the new and old index
-      var start = ui.item.data('start');
-      var end = ui.item.index();
+		function dragEnd(e, ui) {
+			// Get the new and old index
+			var start = ui.item.data('start');
+			var end = ui.item.index();
 
-      // Move the item to its new index
-      $scope.reviewedTeams.splice(end, 0, $scope.reviewedTeams.splice(start, 1)[0]);
+			// Move the item to its new index
+			$scope.reviewedTeams.splice(end, 0, $scope.reviewedTeams.splice(start, 1)[0]);
 
-      var min = Math.min(start, end);
-      var max = Math.max(start, end);
-      for (var i = min; i <= max; i++) {
-        var team = $scope.reviewedTeams[i];
-        // Update the team's rank in the review and in the model
-        team.reviews[$rootScope.user].rank = i + 1;
-        teamsRef.child(`${team.$id}/reviews/${$rootScope.user}`).update({ rank: i + 1 });
-        team.moved = true;
-      }
+			var min = Math.min(start, end);
+			var max = Math.max(start, end);
+			for (var i = min; i <= max; i++) {
+				var team = $scope.reviewedTeams[i];
+				// Update the team's rank in the review and in the model
+				team.reviews[$rootScope.user].rank = i + 1;
+				teamsRef.child(`${team.$id}/reviews/${$rootScope.user}`).update({ rank: i + 1 });
+				team.moved = true;
+			}
 
-      $timeout(function () {
-        for (var i = 0; i < $scope.reviewedTeams.length; i++) {
-          $scope.reviewedTeams[i].moved = false;
-        }
-      }, 1000);
-    }
+			$timeout(function () {
+				for (var i = 0; i < $scope.reviewedTeams.length; i++) {
+					$scope.reviewedTeams[i].moved = false;
+				}
+			}, 1000);
+		}
 
-    var sortableOptions = {
-      start: dragStart,
-      update: dragEnd
-    };
+		var sortableOptions = {
+			start: dragStart,
+			update: dragEnd
+		};
 
-    // Drag and drop only with the handle on small screens
-    if ($scope.isSmallScreen) {
-      sortableOptions.handle = '.drag-handle';
-    }
+		// Drag and drop only with the handle on small screens
+		if ($scope.isSmallScreen) {
+			sortableOptions.handle = '.drag-handle';
+		}
 
-    $('#sortable').sortable(sortableOptions);
-  });
+		$('#sortable').sortable(sortableOptions);
+	});
--- app/summary/summary.js
+++ app/summary/summary.js
@@ -1,145 +1,145 @@
 'use strict';
 angular
-  .module('PitchEvaluator')
-  .controller('SummaryCtrl', function ($rootScope, $scope, permissionsService, $firebaseObject, $firebaseArray, $location, loggedinCheck, teamService) {
-    loggedinCheck.check();
+	.module('PitchEvaluator')
+	.controller('SummaryCtrl', function ($rootScope, $scope, permissionsService, $firebaseObject, $firebaseArray, $location, loggedinCheck, teamService) {
+		loggedinCheck.check();
 
-    if (!permissionsService.isPermitted('Overview')) {
-      if ($rootScope.role == 'Team') {
-        $location.path('team');
-      }
-    }
+		if (!permissionsService.isPermitted('Overview')) {
+			if ($rootScope.role == 'Team') {
+				$location.path('team');
+			}
+		}
 
-    $scope.reviewTeam = function (team) {
-      if ($rootScope.role === 'Judge') {
-        teamService.set(team);
-        $location.path('review');
-      }
-    }
+		$scope.reviewTeam = function (team) {
+			if ($rootScope.role === 'Judge') {
+				teamService.set(team);
+				$location.path('review');
+			}
+		}
 
-    var sessionRef = firebase.database().ref($rootScope.sessionRef);
-    $scope.sessionAverages = $firebaseObject(sessionRef.child('averages'));
-    var teamsRef = firebase.database().ref($rootScope.sessionRef + "/teams");
-    $scope.teams = $firebaseArray(teamsRef);
+		var sessionRef = firebase.database().ref($rootScope.sessionRef);
+		$scope.sessionAverages = $firebaseObject(sessionRef.child('averages'));
+		var teamsRef = firebase.database().ref($rootScope.sessionRef + "/teams");
+		$scope.teams = $firebaseArray(teamsRef);
 
-    $scope.teams.$loaded(function () {
-      $scope.teams.sort(function (a, b) { return a.averages['rank'] - b.averages['rank'] });
-    });
+		$scope.teams.$loaded(function () {
+			$scope.teams.sort(function (a, b) { return a.averages['rank'] - b.averages['rank'] });
+		});
 
-    // Initialize CSV headers for summary and detail CSVs
-    var summaryHeader = ["name", "rank", "average"];
-    var detailHeader = ["name", "reviewer", "rank", "average"];
-    $rootScope.questions.forEach(question => {
-      var label = question.label;
-      summaryHeader.push(label);
-      detailHeader.push(label);
-      detailHeader.push(label) + ' (comments)';
-    });
+		// Initialize CSV headers for summary and detail CSVs
+		var summaryHeader = ["name", "rank", "average"];
+		var detailHeader = ["name", "reviewer", "rank", "average"];
+		$rootScope.questions.forEach(question => {
+			var label = question.label;
+			summaryHeader.push(label);
+			detailHeader.push(label);
+			detailHeader.push(label) + ' (comments)';
+		});
 
-    // Takes an array of columns and an array of data objects
-    // Outputs a CSV string with data header
-    function convertDataToCSV(columns, data) {
-      var columnDelimiter = ',';
-      var lineDelimiter = '\n';
+		// Takes an array of columns and an array of data objects
+		// Outputs a CSV string with data header
+		function convertDataToCSV(columns, data) {
+			var columnDelimiter = ',';
+			var lineDelimiter = '\n';
 
-      var result = 'data:text/csv;charset=utf-8,';
-      result += columns.join(columnDelimiter);
-      result += lineDelimiter;
+			var result = 'data:text/csv;charset=utf-8,';
+			result += columns.join(columnDelimiter);
+			result += lineDelimiter;
 
-      data.forEach(function (item) {
-        columns.forEach(function (key, i) {
-          if (i > 0) result += columnDelimiter;
-          result += item[key];
-        });
-        result += lineDelimiter;
-      });
+			data.forEach(function (item) {
+				columns.forEach(function (key, i) {
+					if (i > 0) result += columnDelimiter;
+					result += item[key];
+				});
+				result += lineDelimiter;
+			});
 
-      return result;
-    }
+			return result;
+		}
 
-    function downloadCSV(csvData, fileName) {
-      var encoded = encodeURI(csvData);
+		function downloadCSV(csvData, fileName) {
+			var encoded = encodeURI(csvData);
 
-      var link = document.createElement('a');
-      link.setAttribute('href', encoded);
-      link.setAttribute('download', fileName);
+			var link = document.createElement('a');
+			link.setAttribute('href', encoded);
+			link.setAttribute('download', fileName);
 
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-    }
+			document.body.appendChild(link);
+			link.click();
+			document.body.removeChild(link);
+		}
 
-    $scope.downloadSummary = function () {
-      Promise.all([$scope.sessionAverages.$loaded(), $scope.teams.$loaded()]).then(() => {
-        var summaryData = [];
+		$scope.downloadSummary = function () {
+			Promise.all([$scope.sessionAverages.$loaded(), $scope.teams.$loaded()]).then(() => {
+				var summaryData = [];
 
-        $scope.teams.forEach(team => {
-          var data = {
-            [summaryHeader[0]]: team.name,
-            [summaryHeader[1]]: team.averages['rank'],
-            [summaryHeader[2]]: team.averages['overall'],
-          }
+				$scope.teams.forEach(team => {
+					var data = {
+						[summaryHeader[0]]: team.name,
+						[summaryHeader[1]]: team.averages['rank'],
+						[summaryHeader[2]]: team.averages['overall'],
+					}
 
-          $rootScope.questions.forEach(question => {
-            var label = question.label;
-            data[label] = team.averages[label];
-          });
+					$rootScope.questions.forEach(question => {
+						var label = question.label;
+						data[label] = team.averages[label];
+					});
 
-          summaryData.push(data);
-        });
+					summaryData.push(data);
+				});
 
-        var classData = {
-          [summaryHeader[0]]: "Class",
-          [summaryHeader[1]]: $scope.sessionAverages['rank'],
-          [summaryHeader[2]]: $scope.sessionAverages['overall'],
-        }
+				var classData = {
+					[summaryHeader[0]]: "Class",
+					[summaryHeader[1]]: $scope.sessionAverages['rank'],
+					[summaryHeader[2]]: $scope.sessionAverages['overall'],
+				}
 
-        $rootScope.questions.forEach(question => {
-          var label = question.label;
-          classData[label] = $scope.sessionAverages[label];
-        });
+				$rootScope.questions.forEach(question => {
+					var label = question.label;
+					classData[label] = $scope.sessionAverages[label];
+				});
 
-        summaryData.push(classData);
+				summaryData.push(classData);
 
-        downloadCSV(convertDataToCSV(summaryHeader, summaryData),
-          "summary.csv");
-      });
-    }
+				downloadCSV(convertDataToCSV(summaryHeader, summaryData),
+					"summary.csv");
+			});
+		}
 
-    $scope.downloadReviews = function (teamName) {
-      $scope.teams.$loaded(() => {
-        var allReviews = [];
+		$scope.downloadReviews = function (teamName) {
+			$scope.teams.$loaded(() => {
+				var allReviews = [];
 
-        $scope.teams.forEach(team => {
-          // Only get data for a certain team if specified
-          if (teamName !== undefined && team.name !== teamName) return;
-          for (var reviewer in team.reviews) {
-            var review = team.reviews[reviewer];
-            var data = {
-              [detailHeader[0]]: team.name,
-              [detailHeader[1]]: reviewer,
-              [detailHeader[2]]: review.rank,
-              [detailHeader[3]]: review.average,
-            }
+				$scope.teams.forEach(team => {
+					// Only get data for a certain team if specified
+					if (teamName !== undefined && team.name !== teamName) return;
+					for (var reviewer in team.reviews) {
+						var review = team.reviews[reviewer];
+						var data = {
+							[detailHeader[0]]: team.name,
+							[detailHeader[1]]: reviewer,
+							[detailHeader[2]]: review.rank,
+							[detailHeader[3]]: review.average,
+						}
 
-            $rootScope.questions.forEach(question => {
-              var label = question.label;
-              data[label] = review.ratings[label];
-              data[label + " (comments)"] = review.comments[label];
-            });
+						$rootScope.questions.forEach(question => {
+							var label = question.label;
+							data[label] = review.ratings[label];
+							data[label + " (comments)"] = review.comments[label];
+						});
 
-            allReviews.push(data);
-          }
-        });
+						allReviews.push(data);
+					}
+				});
 
-        var fileName;
-        if (teamName !== undefined) {
-          fileName = `${teamName}-reviews.csv`;
-        } else {
-          fileName = "all-reviews.csv";
-        }
+				var fileName;
+				if (teamName !== undefined) {
+					fileName = `${teamName}-reviews.csv`;
+				} else {
+					fileName = "all-reviews.csv";
+				}
 
-        downloadCSV(convertDataToCSV(detailHeader, allReviews), fileName);
-      });
-    }
-  });
+				downloadCSV(convertDataToCSV(detailHeader, allReviews), fileName);
+			});
+		}
+	});
