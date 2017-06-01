3c7f78f09ae8dbb0b643529b54cd76887292a317
paep3nguin <paep3nguin@gmail.com>
2017-05-31 22:18:51 -0500

    Revamp addTeam

--- app/addTeam/addTeam.js
+++ app/addTeam/addTeam.js
@@ -1,100 +1,50 @@
 'use strict';
 angular
-  .module('PitchEvaluator')
-  .controller('addTeamCtrl', function($rootScope, $scope, permissionsService, $firebaseObject, $firebaseArray, $location, db_url,loggedinCheck) {
+	.module('PitchEvaluator')
+	.controller('addTeamCtrl', function ($rootScope, $scope, permissionsService, $firebaseObject, $firebaseArray, $location, db_url, loggedinCheck) {
+		loggedinCheck.check();
 
-    loggedinCheck.check();
-    if (!permissionsService.isPermitted('addTeam')) {
-      $location.path('summary');
-    }
+		if (!permissionsService.isPermitted('addTeam')) {
+			$location.path('summary');
+		}
 
-    $scope.name = "";
-    $scope.product = "";
-    $scope.desc = "";
-    $scope.teamPass = ""
-    $scope.warning = false;
+		$scope.name = "";
+		$scope.product = "";
+		$scope.desc = "";
+		$scope.teamPass = ""
 
-    class Team {
-      constructor(name, product, desc, teamPass) {
-        this.name = name;
-        this.product = product;
-        this.desc = desc;
-        this.teamPass = teamPass;
-        this.startRank = 0;
-        this.reviewed = false;
-      }
-      parse() {
-        var temp = {
-          name: this.name,
-          product: this.product,
-          desc: this.desc,
-        }
-        return temp;
-      }
-    }
+		function showWarning(text) {
+			$scope.alertVisible = true;
+			$scope.alertRole = 'danger';
+			$scope.alertMessage = text;
+		}
 
-    $scope.addTeam = function() {
+		$scope.addTeam = function () {
+			if (!$rootScope.session) {
+				showWarning("Invalid or No Session");
+				return;
+			}
 
-      if (!$rootScope.session) {
-        $scope.warning = true;
-        document.querySelector("#warning").innerHTML = "*Invalid or No Session";
-        throw new Error('Invalid or No Session');
-        // return;
-      }
-      var newTeam = new Team($scope.name,$scope.product,$scope.desc, $scope.teamPass);
-      var ref=firebase.database().ref($rootScope.sessionRef);
-      //var ref = new Firebase($rootScope.sessionRef);
-      ref.once("value", function(snapshot) {
-        var count = snapshot.child("teams").numChildren();//
-        newTeam.startRank = count+1;
-        var refTeams=firebase.database().ref($rootScope.sessionRef + "/teams");
-//        var refTeams = new Firebase($rootScope.sessionRef + "/teams")
-        var teams = new $firebaseArray(refTeams);
-        teams.$loaded(function() {
-          var exists = false;
-          teams.forEach(function(team) {
-            if (exists) return;
-            if (team.name==$scope.name) {
-              exists = true;
-              return;
-            }
-          });
-          if (exists) {
-            $scope.warning = true;
-            document.querySelector("#warning").innerHTML = "*Team already exists";
-            throw new Error('Team already exists');
-            // return;
-          }
-          else {
-            $scope.warning = false;
-            ref.child('teams').push(newTeam);
-            $location.path('#/summary');
-          }
-        });
-      });
+			var teamsRef = firebase.database().ref(`${$rootScope.sessionRef}/teams`);
+			var teams = $firebaseArray(teamsRef);
+			teams.$loaded(function () {
+				for (var i = 0; i < teams.length; i++) {
+					if (teams[i].name == $scope.name) {
+						showWarning("Team already exists");
+						return;
+					}
+				}
 
-
-
-      // var ref = new Firebase(db_url + "/sessions");
-      // var sessArr = new $firebaseArray(ref);
-
-      // sessArr.$loaded(function() {
-      //   sessArr.forEach(function(session) {
-      //     if (session.name == $rootScope.session) {
-      //       var newTeam = new Team($scope.name,$scope.product,$scope.desc);
-      //
-      //       var key = sessArr.$keyAt(session);
-      //       var specificSession = new Firebase(ref + "/" + key);
-      //       specificSession.child('teams').push(newTeam);
-      //       $location.path('#/summary');
-      //     }
-      //   });
-      // });
-
-      // console.log($scope.name);
-      // console.log($scope.product);
-      // console.log($scope.desc);
-      // $location.path('#/summary');
-    }
-
-  });
+				teams.$add({
+					name: $scope.name,
+					product: $scope.product,
+					desc: $scope.desc,
+					teamPass: $scope.teamPass
+				}).then(() => {
+					$scope.alertVisible = true;
+					$scope.alertRole = 'success';
+					$scope.alertMessage = "Team added!";
+				});
+			});
+		}
+	});
