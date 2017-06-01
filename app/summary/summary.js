186c5864de09e915125b7fcb974bd2e7d1d088e5
paep3nguin <paep3nguin@gmail.com>
2017-06-01 18:22:13 -0500

    Put unranked teams at end

--- app/summary/summary.js
+++ app/summary/summary.js
@@ -25,10 +25,10 @@ angular
 		$scope.teams.$loaded(function () {
 			$scope.teams.sort(function (a, b) {
 				if (a.averages === undefined || a.averages['rank'] === undefined) {
-					return -1;
+					return 1;
 				}
 				if (b.averages === undefined || b.averages['rank'] === undefined) {
-					return 1;
+					return -1;
 				}
 				return a.averages['rank'] - b.averages['rank'];
 			});
